16ec7052edae902c090155eedefde8842f448501
Marta Kask <marta.m.kask@gmail.com>
2021-04-21 21:27:41 +0200

    first commit of react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+
+  const [tasks, setTasks] = useState (
+    [
+  {
+    id: 1,
+    text: 'dress fitting',
+    day: 'Feb 5th at 4pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'clean car',
+    day: 'Feb 25th at 4:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'meet J',
+    day: 'Feb 29th at 2:30pm',
+    reminder: false,
+  }
+  ])
+
+  // delete task:
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+      <Tasks tasks={tasks} onDelete=
+      {deleteTask}/>
     </div>
+
   );
 }
+
+
+
+
 
 export default App;
