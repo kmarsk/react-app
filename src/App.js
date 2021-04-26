70ceadc9136e89968450bd48e4460527609f3b08
Marta Kask <marta.m.kask@gmail.com>
2021-04-26 17:02:21 +0200

    created add task feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
 
@@ -25,6 +26,13 @@ function App() {
     reminder: false,
   }
   ])
+  // add task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks ([...tasks, newTask])
+
+}
 
   // delete task:
   const deleteTask = (id) => {
@@ -43,6 +51,7 @@ function App() {
   return (
     <div className="container">
       <Header/>
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete=
       {deleteTask} onToggle={toggleReminder}/> ) : ('No Tasks To Show')}
     </div>
